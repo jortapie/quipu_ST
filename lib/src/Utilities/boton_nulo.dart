097cf219740b09c06469e49f4b631e784dbb6c81
Coqui <jortapie@gmail.com>
2021-05-26 08:15:24 -0500

    Modificacion del boton nulo para quitar icono y texto

--- lib/src/Utilities/boton_nulo.dart
+++ lib/src/Utilities/boton_nulo.dart
@@ -17,20 +17,5 @@ Widget crearBotonNulo() {
         )
       ],
     ),
-    child: Container(
-      padding: EdgeInsets.all(30.0),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: <Widget>[
-          SizedBox(height: 15.0,),
-          Container(
-            child: Icon(Icons.handyman, size: 90.0, color: Color.fromRGBO(255, 192, 0, 1.0)),
-          ),
-          SizedBox(height: 10.0,),
-          Text('', style: TextStyle( fontWeight: FontWeight.bold, color: Color.fromRGBO(89, 89, 89, 1.0) ), textAlign: TextAlign.center,),
-          SizedBox(height: 10.0,),
-        ]
-      ),
-    ),
   );
 }
